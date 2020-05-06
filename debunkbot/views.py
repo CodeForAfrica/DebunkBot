83741af0b6f0c36aa2d53d3140665c44dc0b1422
esir <esirkings@gmail.com>
2020-05-06 07:21:12 +0300

    Perform cache of the google sheet data

--- debunkbot/views.py
+++ debunkbot/views.py
@@ -1,7 +1,12 @@
 from django.http import HttpResponse
+from django.core.cache import cache
+from django.conf import settings
+from django.core.cache.backends.base import DEFAULT_TIMEOUT
 
 from utils.gsheet.helper import GoogleSheetHelper
 
+
+CACHE_TTL = getattr(settings, 'CACHE_TTL', DEFAULT_TIMEOUT)
 
 def write_to_gsheet(request):
     sheet = GoogleSheetHelper()
@@ -9,3 +14,13 @@ def write_to_gsheet(request):
     sheet.append_row(new_row)
     sheet.update_cell_value(3, 5, 'Stargazing')
     return HttpResponse('You can confirm the write operations from your google sheet now!')
+
+def read_gsheet(request):
+    if 'gsheet_data' in cache:
+        gsheet_data = cache.get('gsheet_data')
+    else:
+        sheet_helper = GoogleSheetHelper()
+        gsheet_data = sheet_helper.open_sheet()
+        cache.set('gsheet_data', gsheet_data, timeout=CACHE_TTL)
+    
+    return HttpResponse(f'Data from the Google Sheet {gsheet_data}')
