6eaafc5833e6db04dd8bcbc6131817ef8432f239
esir <esirkings@gmail.com>
2020-05-12 09:00:48 +0300

    - Adds reply_id field to track our reply - Adds an impact field to hold impact of our reply

--- debunkbot/models.py
+++ debunkbot/models.py
@@ -6,6 +6,10 @@ class Tweet(models.Model):
     tweet = JSONField()
     responded = models.BooleanField(default=False)
     processed = models.BooleanField(default=False)
+    reply_id = models.CharField(max_length=255, help_text="The Id of the reply we send to this tweet")
+    impact = JSONField(default=dict)
 
+    def __str__(self):
+        return self.tweet.get('text')
     class Meta:
         db_table = 'tweets'
--- debunkbot/twitter/process_stream.py
+++ debunkbot/twitter/process_stream.py
@@ -8,9 +8,10 @@ def process_stream() -> None:
     tweets = Tweet.objects.filter(processed=False)
     for t in tweets:
         if t.tweet['user']['followers_count'] > 100:
-            api.update_status(
-                "Hello! We have checked this link and the news is false",
+            our_resp = api.update_status(
+                f"Hello @{t.tweet.get('user').get('screen_name')} We have checked this link and the news is false",
                 t.tweet['id'])
+            t.reply_id = our_resp._json.get('id')
             google_sheet = GoogleSheetHelper()
             value = google_sheet.get_cell_value(2, 11) + ', https://twitter.com/' + \
                 t.tweet['user']['screen_name'] + '/status/' + t.tweet['id_str']
