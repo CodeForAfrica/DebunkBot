73fca5efea4aeb92e5cc57c55a94d782edfdc02b
esir <esirkings@gmail.com>
2020-05-14 09:16:43 +0300

    Automatically refresh data from google sheet

---
+++ debunkbot/management/commands/refresh_sheet_data.py
@@ -0,0 +1,28 @@
+import time
+import tweepy
+from django.core.management.base import BaseCommand, CommandError
+from django.core.cache import cache
+from django.conf import settings
+
+from utils.gsheet.helper import GoogleSheetHelper
+from debunkbot.twitter.stream_listener import stream
+
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        while True:
+            gsheet = GoogleSheetHelper()
+            gsheet_data = gsheet.open_sheet()
+
+            pos = 2
+            for row in gsheet_data:
+                row.update({'row': pos})
+                pos+=1
+            
+            cache.set('gsheet_data', gsheet_data, timeout=0)
+            self.stdout.write(self.style.SUCCESS(f'Refreshing google sheet data...'))
+            refresh_tracklist_timeout = int(getattr(settings, 'REFRESH_TRACK_LIST_TIMEOUT'))
+            if refresh_tracklist_timeout > 60:
+                time.sleep(refresh_tracklist_timeout - 60)
+            else:
+                time.sleep(refresh_tracklist_timeout)
