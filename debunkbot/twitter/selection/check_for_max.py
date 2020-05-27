ed8530f281a37e6c4f71ab9bd427cba9bc688453
Samuel Afolaranmi <sammysgame.dev@gmail.com>
2020-05-27 14:05:25 +0100

    create twitter account ignorelist

--- debunkbot/twitter/selection/check_for_max.py
+++ debunkbot/twitter/selection/check_for_max.py
@@ -1,10 +1,16 @@
 from datetime import datetime
 from typing import List, Optional
 
+from django.conf import settings
+
 from debunkbot.models import Tweet
 
 
 def check_for_max(tweets: List[Tweet]) -> Optional[Tweet]:
+    tweets = [tweet
+              for tweet in tweets
+              if tweet.tweet[
+                  'user']['screen_name'] not in settings.DEBUNKBOT_TWITTER_ACCOUNT_IGNORE_LIST]  # type: List[Tweet]
     max_tweet = max(tweets, key=lambda x: x.tweet['weight'])  # type: Tweet
     max_tweets = [tweet for tweet in tweets if tweet.tweet['weight'] == max_tweet.tweet['weight']]  # type: List[Tweet]
     if len(max_tweets) == 0:
