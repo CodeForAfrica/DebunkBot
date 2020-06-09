0a7ee5d836b53f6f4404a9716b8e830124fd1afe
esir <esirkings@gmail.com>
2020-06-09 14:23:52 +0300

    Rename 0005_message to 0005_message_template

--- debunkbot/migrations/0006_gsheetclaimsdatabase.py
+++ debunkbot/migrations/0006_gsheetclaimsdatabase.py
@@ -7,7 +7,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('debunkbot', '0005_message'),
+        ('debunkbot', '0005_message_template'),
     ]
 
     operations = [
