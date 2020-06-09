d707e6796bb50dc40ec6b5d181998ffff9ac2919
Isaiah King'ori <esirkings@gmail.com>
2020-06-09 14:24:33 +0300

    Ft celery pull data from google sheet (#28)
    * Add helper method to open google sheet by key
    
    * Adds a task to periodically pull data from googlesheets
    
    * Adds a handler for pulling and saving claims from different googlesheets
    
    * Code refactor
    
    * Prevent holding a list of claims in memory since we are not using it
    
    * Add claim first appearance column
    
    * Create initial claims using claim_first_appearance
    
    * Rename 0005_message to 0005_message_template

--- debunkbot/models.py
+++ debunkbot/models.py
@@ -59,18 +59,17 @@ class Claim(models.Model):
     claim_date = models.CharField(max_length=255, help_text="The date when the claim was made.")
     claim_location = models.CharField(max_length=255, help_text="The location where the claim was made.")
     claim_first_appearance = models.CharField(max_length=255, null=True, help_text="Link to where the claim first appeared.")
+    claim_appearances = ArrayField(
+        models.CharField(max_length=255), null=True, help_text="Links to where the claims appeared.")
     claim_phrase = models.CharField(max_length=255, null=True, help_text="Claim phrase that we should track.")
     claim_author = models.CharField(max_length=255, help_text="The author of the claim")
     rating = models.BooleanField(default=False, help_text="Is the claim true or false")
-    sheet_row = models.CharField(max_length=255, help_text="The sheet row in which this claim belongs to")
     processed = models.BooleanField(
         default=False,
         help_text="Determines if we've processed tweets related to this claim or not")
 
     def __str__(self):
-        return self.claim_first_appearance or self.claim_phrase or ""
-
-
+        return self.claim_reviewed or ''
 class MessageTemplate(models.Model):
     message_template = models.CharField(max_length=255, help_text="Message template to use for sending reply")
 
@@ -88,6 +87,9 @@ class GSheetClaimsDatabase(models.Model):
     claim_url_column_names = ArrayField(
         models.CharField(max_length=255),
         help_text="List of columns to fetch claim urls from in this specific spreadsheet")
+    claim_first_appearance_column_name = models.CharField(
+        max_length=255,
+        help_text="The column to fetch claim first appearance from in this specific spreadsheet")
     claim_phrase_column_name = models.CharField(
         max_length=255,
         help_text="The column to fetch claim phrases from in this specific spreadsheet")
---
+++ debunkbot/utils/claims_handler.py
@@ -0,0 +1,48 @@
+from debunkbot.models import GSheetClaimsDatabase, Claim
+from debunkbot.utils.gsheet.helper import GoogleSheetHelper
+
+
+def fetch_claims_from_gsheet():
+    claim_databases = GSheetClaimsDatabase.objects.all()
+    google_sheet_helper = GoogleSheetHelper()
+    total_claims = 0
+    for claim_database in claim_databases:
+        sheet = google_sheet_helper.get_sheet(claim_database.key)
+        for worksheet_name in claim_database.worksheets:
+            worksheet = sheet.worksheet(worksheet_name)
+            all_records = worksheet.get_all_records()
+            for record in all_records:
+                claim_rating = record.get(claim_database.claim_rating_column_name)
+                if claim_rating:
+                    rating = claim_rating.upper()
+                    if rating in ['TRUE', 'FALSE']:
+                        record['rating'] = rating == 'TRUE'
+                        get_or_create_claim(claim_database, record)
+                        total_claims += 1
+    return total_claims
+
+def get_or_create_claim(claim_database, record):
+    # gets a claim from the database or creates it if it doesn't exist.
+    claim_first_appearance_column_name = claim_database.claim_first_appearance_column_name
+    claim_first_appearance = record.get(claim_first_appearance_column_name)[:255]
+    if claim_first_appearance:
+        claim, created = Claim.objects.get_or_create(claim_first_appearance=claim_first_appearance)
+    else:
+        # If claim first appearance doesn't exist, use the first claim in the claim appearances as the first claim.
+        claim_first_appearance = record.get(claim_database.claim_url_column_names[0])[:255]
+        claim, created = Claim.objects.get_or_create(claim_first_appearance=claim_first_appearance)
+    appearances = []
+    for claim_appearance_column in claim_database.claim_url_column_names:
+        appearances.append(record.get(claim_appearance_column))
+
+    claim.claim_appearances = appearances
+    claim.claim_reviewed = record.get(claim_database.claim_description_column_name)
+    claim.claim_phrase = record.get(claim_database.claim_phrase_column_name)
+    claim.claim_date = 'N/A'
+    claim.claim_location = record.get(claim_database.claim_location_column_name) or 'N/A'
+    claim.fact_checked_url = record.get(claim_database.claim_debunk_url_column_name) or 'N/A'
+    claim.claim_author = record.get(claim_database.claim_author_column_name) or 'N/A'
+    claim.rating = record.get('rating')
+    claim.save()
+    
+    return claim
