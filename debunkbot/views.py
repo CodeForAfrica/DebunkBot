47eeb4ea205f9dc6bfc6d30baeb6a2edd089bb9a
Samuel Afolaranmi <sammysgame.dev@gmail.com>
2020-06-10 09:19:22 +0100

    update url and response

--- debunkbot/views.py
+++ debunkbot/views.py
@@ -5,4 +5,4 @@ from debunkbot.utils.claims_handler import fetch_claims_from_gsheet
 
 def fetch_gsheet_claims(request) -> HttpResponse:
     fetch_claims_from_gsheet()
-    return HttpResponse('All systems go!')
+    return HttpResponse('Claims updated successfully')
