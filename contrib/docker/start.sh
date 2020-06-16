e74181f428070b4b482055bbd9ab436c2e0af52d
esir <esirkings@gmail.com>
2020-06-16 11:24:20 +0300

    Setup for deployment

--- contrib/docker/start.sh
+++ contrib/docker/start.sh
@@ -3,9 +3,13 @@ python manage.py migrate --noinput                # Apply database migrations
 python manage.py collectstatic --clear --noinput  # Collect static files
 
 # Prepare log files and start outputting logs to stdout
+touch /src/logs/celery.log
 touch /app/logs/gunicorn.log
 touch /app/logs/access.log
 tail -n 0 -f /app/logs/*.log &
+
+celery -A debunkbot worker --app=debunkbot.celeryapp:app -l info &> /src/logs/celery.log  
+celery -A debunkbot beat --app=debunkbot.celeryapp:app -l info &> /src/logs/celery.log  &
 
 # Start Gunicorn processes
 echo Starting Gunicorn.
