72823c2f2cec352f0fbe68b14ce936b851f7db96
Samuel Afolaranmi <sammysgame.dev@gmail.com>
2020-07-14 22:51:04 +0100

    password reset

--- debunkbot/urls.py
+++ debunkbot/urls.py
@@ -15,10 +15,31 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.contrib.auth import views as auth_views
 
 from debunkbot import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('claims/update', views.fetch_gsheet_claims, name='gsheet_fetcher'),
+    path(
+    'admin/password_reset/',
+    auth_views.PasswordResetView.as_view(),
+    name='admin_password_reset',
+    ),
+    path(
+        'admin/password_reset/done/',
+        auth_views.PasswordResetDoneView.as_view(),
+        name='password_reset_done',
+    ),
+    path(
+        'reset/<uidb64>/<token>/',
+        auth_views.PasswordResetConfirmView.as_view(),
+        name='password_reset_confirm',
+    ),
+    path(
+        'reset/done/',
+        auth_views.PasswordResetCompleteView.as_view(),
+        name='password_reset_complete',
+    ),
 ]
