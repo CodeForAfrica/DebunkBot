9ef74c5e4e70bf3299c029df66e68a223d89dcce
esir <esirkings@gmail.com>
2020-07-15 09:53:56 +0300

    Mark google sheet claim database as deleted instead of deleting it. - Only display claim databases that have not been marked as deleted.

--- debunkbot/admin.py
+++ debunkbot/admin.py
@@ -11,13 +11,30 @@ from debunkbot.models import (
     IgnoreListGsheet
 )
 
+@admin.register(GSheetClaimsDatabase)
+class GSheetClaimsDatabaseAdmin(admin.ModelAdmin):
+    def get_queryset(self, request):
+        # Only show deleted claims databases to superusers.
+        qs = super().get_queryset(request)
+        if request.user.is_superuser:
+            return qs
+        return qs.filter(deleted=False)
+    
+    def delete_model(self, request, obj):
+        # Only delete from database if the user is a superuser
+        if request.user.is_superuser:
+            super().delete_model(request, obj)
+        else:
+            obj.deleted = True
+            obj.save()
+        return
+
 admin.site.register([
     Tweet,
     Claim,
     Reply,
     Impact,
     MessageTemplate,
-    GSheetClaimsDatabase,
     GoogleSheetCredentials,
     IgnoreListGsheet
 ])
