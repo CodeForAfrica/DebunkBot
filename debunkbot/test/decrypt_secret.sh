01e0a466af0ed78fe2774483eec48a5b7d9222a9
esir <esirkings@gmail.com>
2020-10-05 08:59:37 +0300

    Remove use of credentials encryption.

--- debunkbot/test/decrypt_secret.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# Decrypt the file
-
-# --batch to prevent interactive command
-# --yes to assume "yes" for questions
-ls /
-gpg --quiet --batch --yes --decrypt --passphrase="$CREDENTIALS_SECRET_PASSPHRASE" \
---output ./debunkbot/test/credentials.json ./debunkbot/test/credentials.json.gpg
