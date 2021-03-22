75546ad5b03e05eed0623328aeab20d1c38483e9
esir <esirkings@gmail.com>
2021-03-22 12:57:30 +0300

    Adds celery-slack

--- debunkbot/celeryapp.py
+++ debunkbot/celeryapp.py
@@ -3,6 +3,7 @@ from __future__ import absolute_import
 import os
 
 from celery import Celery
+from celery_slack import Slackify
 from django.conf import settings
 
 # set the default Django settings module for the 'celery' program.
@@ -11,3 +12,11 @@ app = Celery("debunkbot")
 
 app.config_from_object("django.conf:settings")
 app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
+
+SLACK_WEBHOOK = os.environ.get("DEBUNKBOT_CELERY_SLACK_WEBHOOK", "")
+
+options = {
+    # Some subset of options
+    "failures_only": True,
+}
+slack_app = Slackify(app, SLACK_WEBHOOK, **options)
