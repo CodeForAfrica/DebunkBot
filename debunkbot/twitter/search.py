c8320e58bbf4627ba2a708ef64b9974251079c6f
esir <esirkings@gmail.com>
2021-08-19 12:34:47 +0300

    Remove code duplication

--- debunkbot/twitter/search.py
+++ debunkbot/twitter/search.py
@@ -7,90 +7,71 @@ def get_claims_to_search():
     claims = []
     database_priority = DatabasePriority.objects.first()
 
-    low_priority_claims_databases = ClaimsDatabase.objects.filter(
-        deleted=False, priority="low"
-    )
-    normal_priority_claims_databases = ClaimsDatabase.objects.filter(
-        deleted=False, priority="normal"
-    )
-    high_priority_claims_databases = ClaimsDatabase.objects.filter(
-        deleted=False, priority="high"
-    )
+    priority_databases = {
+        "low": ClaimsDatabase.objects.filter(deleted=False, priority="low"),
+        "normal": ClaimsDatabase.objects.filter(deleted=False, priority="normal"),
+        "high": ClaimsDatabase.objects.filter(deleted=False, priority="high"),
+    }
 
-    low_priority_count = normal_priority_count = high_priority_count = 0
-    if low_priority_claims_databases.count() == 0:
-        if normal_priority_claims_databases.count() > 0:
-            normal_priority_count += int(
-                database_priority.normal
-                / (database_priority.normal + database_priority.high)
-                * database_priority.low
-            )
-        if high_priority_claims_databases.count() > 0:
-            high_priority_count += int(
-                database_priority.high
-                / (database_priority.normal + database_priority.high)
-                * database_priority.low
-            )
+    priority_count = {
+        "low": database_priority.low,
+        "normal": database_priority.normal,
+        "high": database_priority.high,
+    }
 
-    if normal_priority_claims_databases.count() == 0:
-        if low_priority_claims_databases.count() > 0:
-            low_priority_count += int(
-                database_priority.low
-                / (database_priority.low + database_priority.high)
-                * database_priority.normal
-            )
-        if high_priority_claims_databases.count() > 0:
-            high_priority_count += int(
-                database_priority.high
-                / (database_priority.low + database_priority.high)
-                * database_priority.normal
-            )
+    for priority in priority_count:
+        """
+        For the 3 priority levels, we want to redistribute a database priority \
+            incase no claim database exists with the given priority.
+        """
+        if not priority_databases[priority].count():
+            priority_count[priority] = 0
+            if priority == "low":
+                other_priority = {"normal": "high", "high": "normal"}
+            if priority == "normal":
+                other_priority = {"low": "high", "high": "low"}
+            if priority == "high":
+                other_priority = {"low": "normal", "normal": "low"}
+            for other_priority_key in other_priority:
+                # Incase one of the other databases is available, either
+                # 1. Distribute the original database priority to the other \
+                # databases based on each database priority
+                # or 2. If the 3rd database type doesn't exist then allocate all \
+                # the priorities of current database to it.
+                if priority_databases[other_priority_key].count():
+                    if priority_databases[other_priority[other_priority_key]]:
+                        priority_count[other_priority_key] += int(
+                            getattr(database_priority, other_priority_key)
+                            / (
+                                getattr(database_priority, other_priority_key)
+                                + getattr(
+                                    database_priority,
+                                    other_priority[other_priority_key],
+                                )
+                            )
+                            * getattr(database_priority, priority)
+                        )
+                    else:
+                        priority_count[other_priority_key] += getattr(
+                            database_priority, priority
+                        )
 
-    if high_priority_claims_databases.count() == 0:
-        if low_priority_claims_databases.count() > 0:
-            low_priority_count += int(
-                database_priority.low
-                / (database_priority.low + database_priority.high)
-                * database_priority.normal
-            )
-        if normal_priority_claims_databases.count() > 0:
-            normal_priority_count += int(
-                database_priority.normal
-                / (database_priority.normal + database_priority.high)
-                * database_priority.high
-            )
-
-    if low_priority_count:
-        low_priority_count = (
-            low_priority_count // low_priority_claims_databases.count()
-        ) * settings.DEBUNKBOT_SEARCHEABLE_CLAIMS_COUNT
-        for claim_database in low_priority_claims_databases:
-            claims.append(
-                claim_database.claims.filter(processed=False, rating=False).values(
-                    "claim_first_appearance"
-                )[:low_priority_count]
-            )
-    if normal_priority_count:
-        normal_priority_count = (
-            normal_priority_count // normal_priority_claims_databases.count()
-        ) * settings.DEBUNKBOT_SEARCHEABLE_CLAIMS_COUNT
-        for claim_database in normal_priority_claims_databases:
-            claims.append(
-                claim_database.claims.filter(processed=False, rating=False).values(
-                    "claim_first_appearance"
-                )[:normal_priority_count]
+    for priority in priority_count:
+        if priority_count[priority]:
+            priority_count[priority] = int(
+                (
+                    (priority_count[priority] / 100)
+                    / priority_databases[priority].count()
+                )
+                * settings.DEBUNKBOT_SEARCHEABLE_CLAIMS_COUNT
             )
 
-    if high_priority_count:
-        high_priority_count = (
-            high_priority_count // high_priority_claims_databases.count()
-        ) * settings.DEBUNKBOT_SEARCHEABLE_CLAIMS_COUNT
-        for claim_database in high_priority_claims_databases:
-            claims.append(
-                claim_database.claims.filter(processed=False, rating=False).values(
-                    "claim_first_appearance"
-                )[:high_priority_count]
-            )
+            for claim_database in priority_databases[priority]:
+                claims.append(
+                    claim_database.claims.filter(processed=False, rating=False).values(
+                        "claim_first_appearance"
+                    )[: priority_count[priority]]
+                )
 
     return claims
 
